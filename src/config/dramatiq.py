dbd89e36a62a8f8bba39749425a2708a51e28ff6
Aryan Tamang <104264310+AryanTmg628@users.noreply.github.com>
2025-08-21 16:49:08 +0545

    refactor : refactor the configuration by using env varaible

--- src/config/dramatiq.py
+++ src/config/dramatiq.py
@@ -2,6 +2,8 @@ import dramatiq
 from dramatiq.brokers.redis import RedisBroker
 from dramatiq.middleware.asyncio import AsyncIO
 
-broker = RedisBroker(url="redis://localhost:6379/0")
+from src.config.settings import settings
+
+broker = RedisBroker(url=settings.REDIS_URL)
 broker.add_middleware(AsyncIO())
 dramatiq.set_broker(broker)
