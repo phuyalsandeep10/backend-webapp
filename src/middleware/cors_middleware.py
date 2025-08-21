f40b9f4b4a2471a0d25f3630daafb4f0d7a85104
sirjan <sirjantmg99@gmail.com>
2025-08-21 06:58:54 +0000

    fixed origin issue

--- src/middleware/cors_middleware.py
+++ src/middleware/cors_middleware.py
@@ -13,25 +13,31 @@ ALLOWED_ORIGINS = [
 
 class CORSMiddleware(BaseHTTPMiddleware):
     def setHeaders(self, response, origin: str):
-        response.headers["Access-Control-Allow-Origin"] = origin
+        if origin:
+            response.headers["Access-Control-Allow-Origin"] = origin
         response.headers["Access-Control-Allow-Methods"] = "GET,POST,PUT,DELETE,OPTIONS"
         response.headers["Access-Control-Allow-Headers"] = "Authorization,Content-Type"
         response.headers["Access-Control-Allow-Credentials"] = "true"
 
     async def dispatch(self, request: Request, call_next):
         origin = request.headers.get("origin")
+        print(f"Request from origin: {origin}")
+        
         path = request.url.path
 
-        # print(f"Request to: {method} {path}")  # <-- log or check endpoint
         response = await call_next(request)
 
         if origin and origin in ALLOWED_ORIGINS:
-            self.setHeaders(response, origin)
+            # self.setHeaders(response, origin)
+            return response
+
+        if not origin:
+            # Don't set headers if origin is None
             return response
 
         if path.startswith("/customer"):
             org = await Organization.find_one({"id": request.headers.get("X-Org-ID")})
-            if origin == org.domain:
+            if org and origin == org.domain:
                 self.setHeaders(response, origin)
                 TenantContext.set(org.id)
                 return response
