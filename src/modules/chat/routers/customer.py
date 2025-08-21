4fd5b2a56cf4fc0201ca90eb2fe36911fa574471
Sirjan Tamang <56628702+sirjandrn25@users.noreply.github.com>
2025-08-21 09:22:42 +0545

    ip log

--- src/modules/chat/routers/customer.py
+++ src/modules/chat/routers/customer.py
@@ -14,10 +14,14 @@ router = APIRouter()
 
 
 @router.post("/{organizationId}")
-async def create_customer(organizationId: int, request: Request):
+async def create_customer( request: Request):
+    organizationId = TenantContext.get()
+    
     header = request.headers.get("X-Forwarded-For")
 
     ip = header.split(",")[0].strip() if header else request.client.host
+
+    print(f"create customer api {ip}")
 
     customer = await Customer.find_one(
         where={"organization_id": organizationId, "ip_address": ip}
@@ -100,6 +104,7 @@ async def save_log(ip: str, customer_id: int, request):
 async def customer_visit(customer_id: int, request: Request):
     header = request.headers.get("X-Forwarded-For")
     ip = header.split(",")[0].strip() if header else request.client.host
+    print(f"visit api {ip}")
 
     customer = await Customer.get(customer_id)
     if not customer:
@@ -117,13 +122,7 @@ async def get_conversation_messages(conversation_id: int):
     return cr.success(data={"messages": [msg.to_json() for msg in messages]})
 
 
-@router.get("")
-async def get_customers(organizationId: int, user=Depends(get_current_user)):
 
-    customers = await Customer.filter(where={"organization_id": organizationId})
-    new_customers = [cus.to_json() for cus in customers]
-
-    return cr.success(data=new_customers)
 
 @router.post('/conversations/{conversation_id}/messages')
 async def create_conversation_message(conversation_id: int, payload: MessageSchema):
