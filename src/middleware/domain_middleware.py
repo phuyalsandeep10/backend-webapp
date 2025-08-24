bc85bcee6d1bf675f6b620773e49b0c53f97d703
Sirjan Tamang <56628702+sirjandrn25@users.noreply.github.com>
2025-08-24 16:34:27 +0545

    fixed issue

--- src/middleware/domain_middleware.py
+++ src/middleware/domain_middleware.py
@@ -13,7 +13,7 @@ class DomainMiddleware(BaseHTTPMiddleware):
             "http://127.0.0.1:8000",
             "http://localhost:8000",
             "https://api.chatboq.com",
-            "https://www.portal.chatboq.com"
+            "https://portal.chatboq.com"
         ]
 
     async def dispatch(self, request: Request, call_next):
